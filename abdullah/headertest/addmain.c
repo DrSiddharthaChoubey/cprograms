f481929d33883ac4af2a22d973e36f0728f33afc
elric220 <abdullah19yr@gmail.com>
2018-10-06 03:43:21 -0400

    abdullah

--- abdullah/headertest/addmain.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include"add.h"
-int main()
-{ int a,b;
-printf("enter the value of and b : ");
-scanf("%d%d",&a,&b);
-printf("sum = %d\n",add(a,b));
-return 0;
-}
--- abdullah/headertest/submain.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include"sub.h"
-int main(){
-int a,b;
-printf("enter the two number : ");
-scanf("%d%d",&a,&b);
-printf("subtract = %d",sub(a,b));
-return 0;
-}
